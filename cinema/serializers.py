43cdfca8cb414165f092c560cb872b2cea06ced3
Silveira <gabriel.silveira.700@ufrn.edu.br>
2026-03-18 07:55:07 -0300

    feat(session): implement SessionSerializer for movie screenings

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -5,3 +5,8 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ['id', 'title', 'description', 'duration_minutes']
+
+class SessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Session
+        fields = ['id', 'movie', 'room', 'start_time']
