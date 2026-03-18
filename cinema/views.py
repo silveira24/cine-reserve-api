087e09a9a4a9b56be38b80b79b9ab158c4fa7696
Silveira <gabriel.silveira.700@ufrn.edu.br>
2026-03-18 09:46:11 -0300

    refactor(movie): remove authentication requirement for Movie list view

--- cinema/views.py
+++ cinema/views.py
@@ -1,11 +1,15 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Movie, Room, Seat, Session, Ticket
 from .serializers import MovieSerializer
 
 from django.utils import timezone
 
+from drf_spectacular.utils import extend_schema
+
+@extend_schema(auth=[], responses={200: MovieSerializer})
 class MovieList(generics.ListAPIView):
     serializer_class = MovieSerializer
+    permission_classes=[permissions.AllowAny,]
 
     def get_queryset(self):
         now = timezone.now()
