0fa5da1fda93c8bf35722f2299aae2a83a5cb4dd
Silveira <gabriel.silveira.700@ufrn.edu.br>
2026-03-17 21:11:07 -0300

    refactor: increase max_length for title field

--- cinema/models.py
+++ cinema/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from accounts.models import User
 
 class Movie(models.Model):
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=100)
     description = models.TextField()
     duration_minutes = models.PositiveIntegerField()
     is_active = models.BooleanField(default=True)
