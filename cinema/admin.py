dfefd5e050768d9b4237e59e5de9adb915756f30
Silveira <gabriel.silveira.700@ufrn.edu.br>
2026-03-18 07:53:04 -0300

    feat(admin): register all cinema models in admin panel

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Room, Seat, Session, Ticket
 # Register your models here.
 
 admin.site.register(Movie)
+admin.site.register(Room)
+admin.site.register(Seat)
+admin.site.register(Session)
+admin.site.register(Ticket)
