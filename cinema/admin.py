e119f7e043befcff0c97270a9137304dd6d9d86c
Silveira <gabriel.silveira.700@ufrn.edu.br>
2026-03-18 23:46:31 -0300

    feat(admin): enhance cinema models with list_display, search and filters

--- cinema/admin.py
+++ cinema/admin.py
@@ -2,8 +2,21 @@ from django.contrib import admin
 from .models import Movie, Room, Seat, Session, Ticket
 # Register your models here.
 
-admin.site.register(Movie)
-admin.site.register(Room)
-admin.site.register(Seat)
-admin.site.register(Session)
-admin.site.register(Ticket)
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'duration_minutes')
+    search_fields = ('title',)
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'rows', 'columns')
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'movie', 'room', 'start_time')
+    list_filter = ('movie', 'room', 'start_time')
+
+@admin.register(Seat)
+class SeatAdmin(admin.ModelAdmin):
+    list_display = ('id', 'row', 'column', 'room')
+    list_filter = ('room',)
